1ff70676d01561c75aa601da645d91b80ffc904b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-09 11:30:20 +0200

    basic setup of my express app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("node:path");
+dotenv.config();
+
+const indexRouter = require("./routes/indexRouter");
+const PORT = process.env.PORT;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+
+app.listen(PORT, () => console.log(`App is listening on the ${PORT}`));
