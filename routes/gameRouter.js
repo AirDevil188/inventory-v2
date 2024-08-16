0ee3dc86b31818d40c14a6c420cabd12dcc1d4e5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-16 12:18:55 +0200

    refactored game routes into its own gameRouter

---
+++ routes/gameRouter.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const gameRouter = Router();
+
+const gameController = require("../controllers/gameController");
+
+gameRouter.get("/create-game", gameController.getCreateGameForm);
+
+gameRouter.post("/create-game", gameController.postCreateGameForm);
+
+gameRouter.get("/game/:id", gameController.getGameDetail);
+
+module.exports = gameRouter;
