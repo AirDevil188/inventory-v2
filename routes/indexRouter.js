76edd22078e9deeef730a97cb9aebe433e4af19b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-16 12:25:36 +0200

    refactored indexRouter moved other routes to separate routers

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,17 +3,7 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 const indexController = require("../controllers/indexController");
-const gameController = require("../controllers/gameController");
-const genreController = require("../controllers/genresController");
 
 indexRouter.get("/", indexController.getIndex);
-
-indexRouter.get("/create-game", gameController.getCreateGameForm);
-
-indexRouter.post("/create-game", gameController.postCreateGameForm);
-
-indexRouter.get("/game/:id", gameController.getGameDetail);
-
-indexRouter.get("/genres", genreController.getGenres);
 
 module.exports = indexRouter;
