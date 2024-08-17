3a4c28b7b6d2e27660ef7d58f03aa68c7bf99d80
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-17 22:15:14 +0200

    created get middleware for create genre form

--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -3,6 +3,9 @@ const { Router } = require("express");
 const genreRouter = Router();
 
 const genreController = require("../controllers/genreController");
+
 genreRouter.get("/", genreController.getGenres);
+
+genreRouter.get("/create-genre", genreController.createGenreFormGet);
 
 module.exports = genreRouter;
