a3490acd2a91704ca3772a5c85fd9b9c30fd53f2
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-18 00:36:12 +0200

    added route for get and post for creating platform

--- routes/platformRouter.js
+++ routes/platformRouter.js
@@ -6,4 +6,14 @@ const platformController = require("../controllers/platformController");
 
 platformRouter.get("/", platformController.getPlatforms);
 
+platformRouter.get(
+  "/create-platform",
+  platformController.getCreatePlatformForm
+);
+
+platformRouter.post(
+  "/create-platform",
+  platformController.postCreatePlatformForm
+);
+
 module.exports = platformRouter;
