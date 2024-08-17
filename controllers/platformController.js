8c5a3655cc9fd43a1f522ede84b3c71019729ca2
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-18 00:35:07 +0200

    added getCreatePlatformForm and getPostPlatformForm middlewers with form validation

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -3,6 +3,12 @@ const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 const indexController = require("../controllers/indexController");
 
+const lengthErr = "must contain at least one character.";
+
+const validatePlatform = [
+  body("name").trim().isLength({ min: 1 }).withMessage(`Name ${lengthErr}`),
+];
+
 const getPlatforms = asyncHandler(async (req, res, next) => {
   const platforms = await db.getPlatforms();
 
@@ -13,6 +19,35 @@ const getPlatforms = asyncHandler(async (req, res, next) => {
   });
 });
 
+const getCreatePlatformForm = asyncHandler(async (req, res, next) => {
+  res.render("platform_form", {
+    title: "Create Platform",
+    navLinks: indexController.navLinks,
+  });
+});
+
+const postCreatePlatformForm = [
+  validatePlatform,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("platform_form", {
+        title: "Create Platform",
+        navLinks: indexController.navLinks,
+        errors: errors.array(),
+      });
+    }
+
+    const { name } = req.body;
+    await db.insertPlatform(name);
+
+    res.redirect("/");
+  }),
+];
 module.exports = {
   getPlatforms,
+  getCreatePlatformForm,
+  postCreatePlatformForm,
 };
