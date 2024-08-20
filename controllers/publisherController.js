3e956f7d89dc175ee4811961695108170bf6b012
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-20 22:18:59 +0200

    changed naming of middlewares and addedform validation with express-validator and new middleware for post route for creating publishers

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -3,6 +3,18 @@ const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 const indexController = require("../controllers/indexController");
 
+const lengthErr = "must contain at least one character.";
+const emptyErr = "must not be empty.";
+
+const validatePublisher = [
+  body("name").trim().isLength({ min: 1 }).withMessage(`Name ${lengthErr}`),
+  body("location")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`Location ${lengthErr}`),
+  body("founded").trim().notEmpty().withMessage(`Date ${emptyErr}`),
+];
+
 const getPublishers = asyncHandler(async (req, res, next) => {
   const publishers = await db.getPublishers();
 
@@ -13,11 +25,35 @@ const getPublishers = asyncHandler(async (req, res, next) => {
   });
 });
 
-const getCreatePublisherGet = asyncHandler(async (req, res, next) => {
+const getCreatePublisherForm = asyncHandler(async (req, res, next) => {
   res.render("publisher_form", {
     title: "Create Publisher",
     navLinks: indexController.navLinks,
   });
 });
 
-module.exports = { getPublishers, getCreatePublisherGet };
+const postCreatePublisherForm = [
+  validatePublisher,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("publisher_form", {
+        title: "Create Publisher",
+        navLinks: indexController.navLinks,
+        errors: errors.array(),
+      });
+    }
+    const { name, location, founded, closed } = req.body;
+
+    console.log(name, location, founded, closed);
+    await db.insertPublisher(name, location, founded, closed);
+    res.redirect("/");
+  }),
+];
+
+module.exports = {
+  getPublishers,
+  getCreatePublisherForm,
+  postCreatePublisherForm,
+};
