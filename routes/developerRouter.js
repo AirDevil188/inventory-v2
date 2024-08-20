f9c50f9b32fb1da48572f07bc0b65b8a3ee9f813
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-20 22:37:48 +0200

    added get route for developerForm

--- routes/developerRouter.js
+++ routes/developerRouter.js
@@ -6,4 +6,9 @@ const developerController = require("../controllers/developerController");
 
 developerRouter.get("/", developerController.getDevelopers);
 
+developerRouter.get(
+  "/create-developer",
+  developerController.getDeveloperCreateForm
+);
+
 module.exports = developerRouter;
