d9705105cb9cb5a69e957ab57afdb440d38e6271
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-21 13:29:18 +0200

    added postDeveloperCreateForm middleware with error validation

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -6,6 +6,18 @@ const indexController = require("./indexController");
 const lengthErr = "must contain at least one character.";
 const selectionErr = "must be selected.";
 
+const validateDeveloper = [
+  body("name").trim().isLength({ min: 1 }).withMessage(`Name ${lengthErr}`),
+  body("location")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage(`Location ${lengthErr}`),
+  body("founded")
+    .trim()
+    .notEmpty({ min: 1 })
+    .withMessage(`Date ${selectionErr}`),
+];
+
 const getDevelopers = asyncHandler(async (req, res, next) => {
   const developers = await db.getDevelopers();
 
@@ -26,7 +38,30 @@ const getDeveloperCreateForm = asyncHandler(async (req, res, next) => {
   });
 });
 
+const postDeveloperCreateForm = [
+  validateDeveloper,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const publishers = await db.getPublishers();
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("developer_form", {
+        title: "Create Developer",
+        navLinks: indexController.navLinks,
+        publishers: publishers,
+        errors: errors.array(),
+      });
+    }
+
+    const { name, location, founded, closed, publisher } = req.body;
+    console.log(publisher);
+    await db.insertDeveloper(name, location, founded, closed, publisher);
+
+    res.redirect("/");
+  }),
+];
 module.exports = {
   getDevelopers,
   getDeveloperCreateForm,
+  postDeveloperCreateForm,
 };
