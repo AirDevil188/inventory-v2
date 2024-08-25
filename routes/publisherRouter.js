2f997516b1a0dfd0df4faaffe329cedd509ba712
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-25 23:22:31 +0200

    added two routes get ad post for deleting publisher

--- routes/publisherRouter.js
+++ routes/publisherRouter.js
@@ -15,4 +15,15 @@ publisherRouter.post(
 );
 
 publisherRouter.get("/publisher/:id", publisherController.getPublisherDetails);
+
+publisherRouter.get(
+  "/publisher/:id/delete",
+  publisherController.getPublisherDelete
+);
+
+publisherRouter.post(
+  "/publisher/:id/delete",
+  publisherController.postPublisherDelete
+);
+
 module.exports = publisherRouter;
