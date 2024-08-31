22fd0de365ad6c122a8fcab0c53d2e567309f6f7
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-31 20:01:17 +0200

    added static path

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/games", gameRouter);
