7314a3a07b90d74fc14198ac303c5317584a41dc
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-12 17:15:00 +0200

    changed name for game platform table to game_platform

--- db/populateDb.js
+++ db/populateDb.js
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS game
    date_of_release DATE,
    url TEXT GENERATED ALWAYS AS ('/games/game/' || id::text) STORED); 
 
-CREATE TABLE IF NOT EXISTS game_developer (game_id INTEGER REFERENCES game(id), platform_id INTEGER REFERENCES platform(id));
+CREATE TABLE IF NOT EXISTS game_platform (game_id INTEGER REFERENCES game(id), platform_id INTEGER REFERENCES platform(id));
  
 
 
@@ -30,9 +30,7 @@ async function mainDriver() {
   console.log("sending information...");
   const client = new Client({
     connectionString:
-      process.env.STATUS === "production"
-        ? process.env.EXTERNAL_DB_CONNECTION_STRING
-        : process.env.LOCAL_DB_CONNECTION_STRING,
+      "postgres://inventory-app-v2-main-db-0107bfc3cc38e886f:7zVvE4bcNaDKXBEJyyVejRw5zBh8F9@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/inventory-app-v2-main-db-0107bfc3cc38e886f",
   });
   await client.connect();
   await client.query(SQL);
