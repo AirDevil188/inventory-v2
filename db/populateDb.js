ef8467fb46295a7e0948f0c079d8ddb26444f8a1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-12 14:50:32 +0200

    fixed create table queries

--- db/populateDb.js
+++ db/populateDb.js
@@ -2,13 +2,13 @@ const { Client } = require("pg");
 
 const SQL = `
 
-CREATE TABLE IF NOT EXISTS platform(id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, url TEXT GENERATED ALWAYS AS ('/platforms/platform' || id::text) ); 
+CREATE TABLE IF NOT EXISTS platform(id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, url TEXT GENERATED ALWAYS AS ('/games/game' || id::text) STORED); 
 
-CREATE TABLE IF NOT EXISTS genre(id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, url TEXT GENERATED ALWAYS AS ('/genres/genre' || id::text)); 
+CREATE TABLE IF NOT EXISTS genre(id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, url TEXT GENERATED ALWAYS AS ('/genres/genre/' || id::text) STORED); 
 
-CREATE TABLE IF NOT EXISTS publisher (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, location VARCHAR(255), founded DATE, closed BOOLEAN, url TEXT GENERATED ALWAYS AS ('/publishers/publisher/' || id::text));
+CREATE TABLE IF NOT EXISTS publisher (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, location VARCHAR(255), founded DATE, closed BOOLEAN, url TEXT GENERATED ALWAYS AS ('/publishers/publisher/' || id::text) STORED);
 
-CREATE TABLE IF NOT EXISTS developer (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, location VARCHAR(255), founded DATE, closed BOOLEAN, publisher INTEGER REFERENCES publisher(id), url TEXT GENERATED ALWAYS AS ('/developers/developer' || id::text));
+CREATE TABLE IF NOT EXISTS developer (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) UNIQUE, location VARCHAR(255), founded DATE, closed BOOLEAN, publisher INTEGER REFERENCES publisher(id), url TEXT generated ALWAYS AS ('/developers/developer/' || id::text) STORED);
 
 CREATE TABLE IF NOT EXISTS game 
 (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, title VARCHAR(255) UNIQUE, 
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS game
   platform INTEGER REFERENCES platform(id), 
   genre INTEGER REFERENCES genre(id),
    date_of_release DATE,
-   url TEXT GENERATED ALWAYS AS ('/games/game/' || id::text)); 
+   url TEXT GENERATED ALWAYS AS ('/games/game/' || id::text) STORED); 
 
 CREATE TABLE IF NOT EXISTS game_developer (game_id INTEGER REFERENCES game(id), platform_id INTEGER REFERENCES platform(id));
  
